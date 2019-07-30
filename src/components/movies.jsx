c409dcbe6d0319e10597eac3342f4eba96221d5e
Vien_Hong <vienhong@Vien-Hongs-MacBook-Pro.local>
2019-07-30 11:22:49 -0400

    passing the data to the pagination

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { getMovies } from "../services/fakeMovieService";
 import Like from "./common/Like";
 import Pagination from "./common/pagination";
+import { paginate } from "../utils/paginate";
 class Movies extends Component {
   state = {
     movies: getMovies(),
@@ -24,9 +25,10 @@ class Movies extends Component {
   };
   render() {
     const { length: count } = this.state.movies;
-    const { pageSize, currentPage } = this.state;
+    const { pageSize, currentPage, movies: allMovies } = this.state;
     if (count === 0) return <p>There are no movies in the database</p>;
 
+    const movies = paginate(allMovies, currentPage, pageSize);
     return (
       <React.Fragment>
         <p> Showing {count} movies in the database</p>
@@ -43,7 +45,7 @@ class Movies extends Component {
             </tr>
           </thead>
           <tbody>
-            {this.state.movies.map(movie => (
+            {movies.map(movie => (
               <tr key={movie._id}>
                 <td>{movie.title}</td>
                 <td>{movie.genre.name}</td>
